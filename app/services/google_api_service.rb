08c666aae4b2e33bd322232eafc3fe3547d0006b
MackHalliday <mackenzie.halliday@gmail.com>
2019-10-25 10:23:07 -0600

    Create Google Api Service class

---
+++ app/services/google_api_service.rb
@@ -0,0 +1,14 @@
+
+class GoogleApiService
+
+  def get_direction_data((house, destination)
+    json_response = conn.get('json?', key: ENV['GOOGLE_API'], origin: origin, destination: destination)
+    parsed_data = JSON.parse(json_response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(
+      url: 'https://maps.googleapis.com/maps/api/directions/'
+    )
+  end
+end
