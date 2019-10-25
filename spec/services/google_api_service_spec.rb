6e9322b19b819ca22dda9c10a4ff02f5f80faf2a
MackHalliday <mackenzie.halliday@gmail.com>
2019-10-25 10:18:10 -0600

    Updates to google api test call based on actual json response

--- spec/services/google_api_service_spec.rb
+++ spec/services/google_api_service_spec.rb
@@ -3,17 +3,18 @@ require 'rails_helper'
 describe 'google api service' do
   it "google location data" do
 
-    #https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=ENV['GOOGLE_API']
+    #https://maps.googleapis.com/maps/api/directions/json?origin=1331%2017th%20St%20CO%2080202&destination=650%2015th%20St%20CO%2080202&key=ENV['GOOGLE_API']
 
     service = GoogleApiService.new
 
-    raw_data = service.get_location_data("Turing", "")
+    raw_data = service.get_direction_data("1331 17th St CO 80202", "650 15th St CO 80202")
 
     expect(service).to be_a(GoogleApiService)
 
     expect(raw_data).to be_a(Array)
 
-    expect(raw_data[0]).to have_key(:id)
-    expect(raw_data[0]).to have_key(:location)
+    expect(raw_data[:routes]).to have_key(:distance)
+    expect(raw_data[:routes]).to have_key(:steps)
+    expect(raw_data[:routes][:steps]).to have_key(:html_instructions))
   end
 end
